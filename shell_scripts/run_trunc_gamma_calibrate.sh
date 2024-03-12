7dc25494c338131a7690d4ad3cad67a397440d2f
Liz Lawler <eslawler@gmail.com>
2024-03-12 10:51:26 +0000

    Upload compiled model files; fix for loop to run through all 100 files

--- shell_scripts/run_trunc_gamma_calibrate.sh
+++ shell_scripts/run_trunc_gamma_calibrate.sh
@@ -11,12 +11,13 @@ ${stanc_exe} ${object}.stan --include-paths=${inc_path}
 cmdstan_model ${object}
 for level in "low" "mid" "high"
 do
-for i in 1..100
+for i in {1..100}
+do
 export gauge_name level i
 nohup ./shell_scripts/submodel_sampling_calibrate.sh > shell_scripts/console_output/calibrate/${gauge_name}_${level}_${i}.txt 2>&1&
 sleep 10
 done
-sleep 60
+sleep 10
 done
-sleep 60
+sleep 10
 done
