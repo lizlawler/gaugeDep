65d4b98799d8063b57829b34058dfe459a0283d4
Liz Lawler <eslawler@gmail.com>
2024-04-28 19:25:24 -0600

    update shell script to run through all combos of censored models

--- shell_scripts/run_cens_calibrate.sh
+++ shell_scripts/run_cens_calibrate.sh
@@ -4,22 +4,20 @@ trap '' HUP
 stanc_exe="/opt/homebrew/Caskroom/miniconda/base/envs/stan/bin/cmdstan/bin/stanc"
 # compile model and link c++ 
 inc_path="stan/"
-# for gauge_name in "gauss" "logistic" "inv_log" "asym_log" "dirichlet"
-for gauge_name in "gauss"
+for gauge_name in "gauss" "logistic" "inv_log" "asym_log" "dirichlet"
 do
 object="stan/bivar_cens_${gauge_name}"
 ${stanc_exe} ${object}.stan --include-paths=${inc_path}
 cmdstan_model ${object}
-# for level in "low" "mid" "high"
-for level in "high"
+for level in "low" "mid" "high"
 do
-for i in 1
+for i in {1..100}
 do
 export gauge_name level i
 nohup ./shell_scripts/sampling_cens_calibrate.sh > shell_scripts/console_output/calibrate/${gauge_name}_${level}_${i}_cens.txt 2>&1
-sleep 3
+sleep 1
 done
-sleep 2
+sleep 1
 done
-sleep 2
+sleep 1
 done
