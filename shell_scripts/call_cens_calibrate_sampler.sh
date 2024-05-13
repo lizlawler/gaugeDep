c4d79e4e861361ccce7d334ff4d6738721bfe04d
Liz Lawler <eslawler@gmail.com>
2024-05-13 15:38:16 -0600

    run censored likelihood calibration simulations on alpine

---
+++ shell_scripts/call_cens_calibrate_sampler.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --partition=amilan
+#SBATCH --account=csu54_alpine1
+#SBATCH --chdir=/scratch/alpine/eslawler@colostate.edu/gaugeDependence/
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=eslawler@colostate.edu
+
+export TMPDIR=/scratch/alpine/$USER/tmp/
+export TMP=${TMPDIR}
+export TEMP=${TMPDIR}
+export TEMPDIR=${TMPDIR}
+mkdir -p $TMPDIR
+
+source /curc/sw/anaconda3/2023.09/etc/profile.d/conda.sh
+conda activate stan
+
+./shell_scripts/sampling_cens_calibrate_alpine.sh \
+${gauge_name} ${threshold} ${level}
---
+++ shell_scripts/run_cens_calibrate_alpine.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# shell script to call sbatch
+#
+# cycle through loop and launch sbatch for every combination
+#
+source /curc/sw/anaconda3/2023.09/etc/profile.d/conda.sh
+conda activate stan
+
+stanc_exe="/projects/$USER/software/anaconda/envs/stan/bin/cmdstan/bin/stanc"
+# compile model and link c++ 
+inc_path="stan/"
+for gauge_name in "gauss" "logistic" "inv_log" "asym_log" "dirichlet"
+do
+for threshold in "marg" "ctau"
+do
+object="stan/bivar_cens_${threshold}_${gauge_name}"
+${stanc_exe} ${object}.stan --include-paths=${inc_path}
+cmdstan_model ${object}
+for level in "low" "mid" "high"
+do
+export gauge_name threshold level
+sbatch --job-name ${gauge_name}_${threshold}_${level} \
+--output="./shell_scripts/console_output/calibrate/%x_%j.txt" \
+shell_scripts/call_cens_calibrate_sampler.sh
+sleep 1
+done
+sleep 1
+done
+sleep 1
+done
