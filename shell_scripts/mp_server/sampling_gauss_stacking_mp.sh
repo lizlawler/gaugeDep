7cd72e7ebef763be19f07456f1a24872a9bd28e9
Liz Lawler <eslawler@gmail.com>
2024-06-15 16:15:23 -0600

    activate R conda environment to execute 'Rscript' command
    then reactivate stan environment

--- shell_scripts/mp_server/sampling_gauss_stacking_mp.sh
+++ shell_scripts/mp_server/sampling_gauss_stacking_mp.sh
@@ -8,6 +8,7 @@ model="bivar_${likelihood}_${threshold}_${gauge_name}"
 
 # run model with 3 chains
 for level in "low" "mid" "high" "wc"
+echo "Starting model runs with for ${level} dependence of AI datasets"
 do
 for i in {1..100}
 do
@@ -23,8 +24,11 @@ done
 sleep 1
 echo "Model has finished running on all 100 datasets for ${level} of AI datasets"
 sleep 1
+conda activate r_env
 Rscript --vanilla extract_params.R \
 ${dep_type} ${gauge_name} ${likelihood} ${threshold} ${level}
 sleep 1
+conda activate stan_new
 done
-
+sleep 1
+echo "${gauge_name}, with ${likelihood} likelihood and ${threshold} threshold has finished running on all datasets"
