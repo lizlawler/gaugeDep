a4f337db765169ba0152319b6438695947b31579
Liz Lawler <eslawler@gmail.com>
2024-06-17 15:27:59 -0600

    Create separate shell scripts to extract stacking weights for gauss and logistic truncated

---
+++ shell_scripts/mp_server/run_weights_extract_logistic_trunc.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/zsh
+# shell script to kick off sampling
+#
+# cycle through loop and launch sampling for each combination
+#
+source /data/accounts/lawler/.zshrc
+
+conda activate r_env
+trap '' HUP
+likelihood="trunc"
+dep_type="logistic"
+for threshold in "marg" "ctau"
+do
+for level in "low" "mid" "high" "wc_low" "wc_mid"
+do
+nohup Rscript --vanilla extract_weights.R ${dep_type} ${likelihood} ${threshold} ${level} > shell_scripts/console_output/stacking/${dep_type}_${likelihood}_${threshold}_${level}_weights_mp.txt 2>&1 &
+sleep 1
+done
+sleep 1
+done
