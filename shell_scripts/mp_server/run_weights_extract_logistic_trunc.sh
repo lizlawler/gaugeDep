2187d9a9f47386ebc7640acede2393d287452434
Liz Lawler <eslawler@gmail.com>
2024-06-18 08:41:42 -0600

    Extract stacking weights for remaining logistic truncated models

--- shell_scripts/mp_server/run_weights_extract_logistic_trunc.sh
+++ shell_scripts/mp_server/run_weights_extract_logistic_trunc.sh
@@ -9,12 +9,9 @@ conda activate r_env
 trap '' HUP
 likelihood="trunc"
 dep_type="logistic"
-for threshold in "marg" "ctau"
-do
-for level in "low" "mid" "high" "wc_low" "wc_mid"
+threshold="marg"
+for level in "low" "mid"
 do
 nohup Rscript --vanilla extract_weights.R ${dep_type} ${likelihood} ${threshold} ${level} > shell_scripts/console_output/stacking/${dep_type}_${likelihood}_${threshold}_${level}_weights_mp.txt 2>&1 &
-sleep 1
-done
 sleep 1
 done
