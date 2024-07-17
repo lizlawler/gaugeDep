1d5ec71e27fad2da128f5ab7994f08e726485a6e
Liz Lawler <eslawler@gmail.com>
2024-07-17 12:25:25 -0400

    add shell script to run all models on Husler-Reiss data

---
+++ shell_scripts/run_hr_stacking.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# shell script to call sbatch
+#
+# cycle through loop and launch sbatch for every combination
+#
+source /curc/sw/anaconda3/2023.09/etc/profile.d/conda.sh
+conda activate stan
+
+stanc_exe="/projects/$USER/software/anaconda/envs/stan/bin/cmdstan/bin/stanc"
+# compile model and link c++ 
+inc_path="stan/"
+dep_type="husler_reiss"
+threshold="marg"
+for gauge_name in "gauss" "logistic" "inv_log" "asym_log" "dirichlet" "rectangular"
+do
+for likelihood in "trunc" "cens"
+do
+object="stan/bivar_${likelihood}_${threshold}_${gauge_name}"
+${stanc_exe} ${object}.stan --include-paths=${inc_path}
+cmdstan_model ${object}
+for level in "low" "mid" "high"
+do
+export gauge_name likelihood threshold dep_type level
+parentjob=$(sbatch --parsable $1 --job-name ${gauge_name}_${likelihood}_${threshold}_${dep_type}_${level}_hr_sampling \
+--output="./shell_scripts/console_output/stacking/%x_%j.txt" \
+shell_scripts/call_${likelihood}_stacking_sampler.sh)
+sleep 1
+sbatch --dependency=afterok:${parentjob} \
+--job-name ${gauge_name}_${likelihood}_${threshold}_${dep_type}_${level}_hr_params \
+--output="./shell_scripts/console_output/stacking/%x_%j.txt" \
+shell_scripts/call_${likelihood}_stacking_params_extraction.sh
+sleep 1
+done
+sleep 1
+done
+sleep 1
+done
