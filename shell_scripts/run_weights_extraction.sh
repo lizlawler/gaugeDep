80ededcb83a62d5737832f78690f28245991e0c0
Liz Lawler <eslawler@gmail.com>
2024-07-25 09:53:12 -0600

    extract stacking weights for H-R data on alpine

---
+++ shell_scripts/run_weights_extraction.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# shell script to call sbatch
+#
+# cycle through loop and launch sbatch for every combination
+#
+source /curc/sw/anaconda3/2023.09/etc/profile.d/conda.sh
+conda activate lawler
+
+dep_type="husler_reiss"
+threshold="marg"
+for likelihood in "trunc" "cens"
+do
+for level in "low" "mid" "high"
+do
+export likelihood threshold dep_type level
+sbatch --job-name ${dep_type}_${level}_${likelihood}_${threshold}_weights \
+--output="./shell_scripts/console_output/stacking/%x_%j.txt" \
+shell_scripts/call_${likelihood}_stacking_weights_extraction.sh
+sleep 1
+done
+sleep 1
+done
