af14d07236877397d53692ac131706b722b348bb
Liz Lawler <eslawler@gmail.com>
2024-07-29 18:08:42 -0600

    creating separate file that houses gauge functions wrt Euclid coord

---
+++ gauge_functions_wrt_x.R
@@ -0,0 +1,27 @@
+## Gauge functions
+gauss_gauge <- function(x1, x2, dep_par = 0.5) {
+  top <- x1 + x2 - 2 * dep_par * sqrt(x1 * x2)
+  return(top/(1-dep_par^2))
+}
+
+logistic_gauge <- function(x1, x2, dep_par = 0.5) {
+  r_inv <- 1/dep_par
+  return(r_inv * pmax(x1, x2) + (1-r_inv)*pmin(x1, x2))
+}
+
+inv_log_gauge <- function(x1, x2, dep_par = 0.5) ((x1^(1/dep_par) + x2^(1/dep_par))^dep_par)
+
+asym_log_gauge <- function(x1, x2, dep_par = 0.5) {
+  r_inv <- 1/dep_par
+  return(pmin((x1 + x2), (r_inv * pmax(x1, x2) + (1-r_inv) * pmin(x1, x2))))
+}
+
+dirichlet_gauge <- function(x1, x2, dep_par) {
+  theta1 <- dep_par[1]
+  theta2 <- dep_par[2]
+  return((1 + theta1 + theta2) * pmax(x1, x2) - (theta1 * x1 + theta2 * x2))
+}
+
+rectangular_gauge <- function(x1, x2, dep_par) {
+  return(pmax((x1 - x2) / dep_par, (x2 - x1) / dep_par, (x1 + x2) / (2 - dep_par)))
+}
