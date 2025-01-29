f35679614df01d80c534532574e4678279a72f6d
Liz Lawler <eslawler@gmail.com>
2025-01-29 12:46:05 -0700

    create script to join radial and angular loglik and calculate BMA wts

---
+++ extraction_scripts/extract_weights_joint.R
@@ -0,0 +1,101 @@
+args <- commandArgs(trailingOnly=TRUE)
+dep_type <- args[1]
+dep_level <- args[2]
+gauge <- args[3]
+likelihood <- args[4]
+
+library(qs)
+library(posterior)
+library(loo)
+
+options(mc.cores = parallel::detectCores())
+angle_dens <- "sb"
+
+create_joint_loglik <- function(dep_type, dep_level, gauge, data_num, likelihood = "cens", angle_dens = "vol") {
+  trunc <- (likelihood == "trunc")
+  vol <- (angle_dens == "vol")
+  loglik_file <- sprintf("samplers/joint_loglik/%s/%s/%s_%s_%s_%s.qs",
+                         dep_type, angle_dens, gauge, likelihood, dep_level, data_num)
+  
+  # Check if joint loglikelihood file already exists
+  if (file.exists(loglik_file)) {
+    return(qread(loglik_file))
+  }
+  
+  # If file doesn't already exist, create it
+  temp_radial <- qread(sprintf("samplers/rcpp/radial_mcmc_fits/%s/pw_loglik/%s_%s_%s_%s.qs",
+                               dep_type, gauge, likelihood, dep_level, data_num))
+  
+  temp_angular <- if (vol) {
+    qread(sprintf("samplers/rcpp/angular_vol_mcmc_fits/%s/pw_loglik/%s_%s_%s.qs", 
+                  dep_type, gauge, dep_level, data_num))
+  } else {
+    qread(sprintf("samplers/nimble/sb_mcmc_fits/%s/pw_loglik/%s_%s.qs", 
+                  dep_type, dep_level, data_num))
+  }
+  
+  if (trunc) {
+    idx <- RcppSimdJson::fload(sprintf("data/%s/%s_%s.json", dep_type, dep_level, data_num))$idx
+    temp_angular <- temp_angular[, idx]
+  }
+  
+  temp_joint <- temp_radial + temp_angular
+  qsave(temp_joint, loglik_file)
+  temp_joint
+}
+
+extract_lpd_pt <- function(dep_type, dep_level, gauge, data_num, likelihood, angle_dens) {
+  temp <- create_joint_loglik(dep_type = dep_type, dep_level = dep_level, gauge = gauge, 
+                              data_num = data_num, likelihood = likelihood, angle_dens = angle_dens)
+  loo_temp <- loo(temp)
+  return(loo_temp$pointwise[,"elpd_loo"])
+}
+
+create_lpd_list <- function(dep_type, dep_level, data_num, likelihood, angle_dens) {
+  gauge_library <- c("gauss", "logistic", "inv_log", "asym_log", "dirichlet", "rectangular") 
+  lpd_list <- setNames(sapply(gauge_library,
+                              function(x) extract_lpd_pt(dep_type = dep_type, dep_level = dep_level, gauge = x,
+                                                         data_num = data_num, likelihood = likelihood, angle_dens = angle_dens)),
+                       gauge_library)
+  return(lpd_list)
+}
+
+model_weights <- function(dep_type, dep_level, data_num, likelihood, angle_dens) {
+  temp <- create_lpd_list(dep_type = dep_type, dep_level = dep_level, 
+                          data_num = data_num,
+                          likelihood = likelihood, 
+                          angle_dens = angle_dens)
+  stacking <- stacking_weights(temp)
+  pseudobma_boot <- pseudobma_weights(temp)
+  pseudobma_noboot <- pseudobma_weights(temp, BB = FALSE)
+  print(paste0("Weights extracted for dataset: ", dataset_num))
+  return(list("stacking" = stacking,
+              "pseudobma_boot" = pseudobma_boot,
+              "pseudobma_noboot" = pseudobma_noboot))
+}
+
+make_wts_df <- function(dep_type, dep_level, data_num, likelihood, angle_dens) {
+  wts_file <- sprintf("fits_and_weights/wts_joint_model/%s_%s_%s_%s.qs",
+                         dep_type, likelihood, angle_dens, dep_level)
+  
+  # Check if joint loglikelihood file already exists
+  if (file.exists(wts_file)) {
+    return(qread(wts_file))
+  }
+  
+  wts <- lapply(1:100, function(x) model_weights(dep_type = dep_type, dep_level = level, likelihood = likelihood, 
+                                                  threshold = threshold, dataset_num = x))
+  gauge_library <- c("gauss", "logistic", "inv_log", "asym_log", "dirichlet", "rectangular")
+  temp <- wts |>
+    bind_rows() |> 
+    mutate(method = rep(gauge_library, 100)) |>
+    mutate(stacking = as.numeric(stacking),
+           pseudobma_boot = as.numeric(pseudobma_boot),
+           pseudobma_noboot = as.numeric(pseudobma_noboot)) |>
+    mutate(dataset = rep(1:100, times = rep(6, 100)))
+
+  qsave(temp, wts_file)
+  temp_joint
+}
+
+
---
+++ shell_scripts/run_joint_model_wts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# shell script to call sbatch
+#
+# cycle through loop and launch sbatch for every combination
+#
+for dep_type in "gauss" "logistic"; do
+  for level in "low" "mid" "high"; do
+    for gauge_name in "gauss" "logistic" "inv_log" "asym_log" "dirichlet" "rectangular"; do
+      for likelihood in "trunc" "cens"; do
+        export dep_type level gauge_name likelihood
+        sbatch --job-name ${gauge_name}_${likelihood}_${dep_type}_${level}_wts \
+        --output="./shell_scripts/console_output/%x_%j.txt" \
+        shell_scripts/call_joint_model_wts_extract.sh
+        sleep 1
+      done
+      sleep 1
+    done
+    sleep 1
+  done
+  sleep 1
+done
