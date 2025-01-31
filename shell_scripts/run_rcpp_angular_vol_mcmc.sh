e198d786129a71ec37e052a14d17b966644e06df
Liz Lawler <eslawler@gmail.com>
2025-01-30 22:45:22 -0700

    clean up shell script for loop syntax

--- shell_scripts/run_rcpp_angular_vol_mcmc.sh
+++ shell_scripts/run_rcpp_angular_vol_mcmc.sh
@@ -3,19 +3,16 @@
 #
 # cycle through loop and launch sbatch for every combination
 #
-for dep_type in "gauss" "logistic"
-do
-for level in "low" "mid" "high"
-do
-for gauge_name in "gauss" "logistic" "inv_log" "asym_log" "dirichlet" "rectangular"
-do
-export dep_type level gauge_name
-sbatch --job-name ${gauge_name}_${dep_type}_${level}_angle_vol_sampling \
---output="./shell_scripts/console_output/%x_%j.txt" \
-shell_scripts/call_angle_vol_sampler.sh
-sleep 1
-done
-sleep 1
-done
-sleep 1
+for dep_type in "gauss" "logistic"; do
+  for level in "low" "mid" "high"; do
+    for gauge_name in "gauss" "logistic" "inv_log" "asym_log" "dirichlet" "rectangular"; do
+      export dep_type level gauge_name
+      sbatch --job-name ${gauge_name}_${dep_type}_${level}_angle_vol_sampling \
+      --output="./shell_scripts/console_output/%x_%j.txt" \
+      shell_scripts/call_angle_vol_sampler.sh
+      sleep 1
+    done
+    sleep 1
+  done
+  sleep 1
 done
