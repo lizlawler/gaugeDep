8ae221385b8a9dfea4444f3d3b7cdfb4f34e6cb4
Liz Lawler <eslawler@gmail.com>
2025-02-13 13:52:57 -0700

    change 100 to 200 in for loops of samplers; modify file naming schema for consistency

--- samplers/rcpp/ang_star_loglik_calc.R
+++ samplers/rcpp/ang_star_loglik_calc.R
@@ -12,11 +12,9 @@ print(paste0("dep_type = ", dep_type))
 print(paste0("dep_level = ", dep_level))
 print(paste0("gauge = ", gauge))
 
-for(data_num in 1:100) {
+for(data_num in 1:200) {
   datafile <- sprintf("data/%s/%s_%s.json", dep_type, dep_level, data_num)
-  # paramsfile <- sprintf("samplers/rcpp/angular_vol_mcmc_fits/%s/%s_%s_%s.qs",
-  #                       dep_type, gauge, dep_level, data_num)
-  paramsfile <- sprintf("samplers/rcpp/angular_vol_mcmc_fits/%s/%s_%s_%s_wexc.qs",
+  paramsfile <- sprintf("samplers/rcpp/angular_vol_mcmc_fits/%s/%s_%s_%s.qs",
                         dep_type, gauge, dep_level, data_num)
   data <- RcppSimdJson::fload(datafile)
   params <- qread(paramsfile)$samples
@@ -27,9 +25,7 @@ for(data_num in 1:100) {
                             posterior_params = params, 
                             gauge_type = gauge)
   
-  # savename <- sprintf("samplers/rcpp/angular_vol_mcmc_fits/%s/pw_loglik/%s_%s_%s.qs", 
-  #                     dep_type, gauge, dep_level, data_num)
-  savename <- sprintf("samplers/rcpp/angular_vol_mcmc_fits/%s/pw_loglik/%s_%s_%s_wexc.qs", 
+  savename <- sprintf("samplers/rcpp/angular_vol_mcmc_fits/%s/pw_loglik/%s_%s_%s.qs",
                       dep_type, gauge, dep_level, data_num)
   qsave(x = results, file = savename)
   print(paste0("Successfully saved posterior pointwise loglikelihood for dataset number: ", data_num))
