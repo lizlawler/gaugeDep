e19d207e1f383f4f0353684759691a40ef573170
Liz Lawler <eslawler@gmail.com>
2025-02-25 14:00:24 -0700

    increase requested resources for angle star sampler

--- shell_scripts/call_angle_star_sampler.sh
+++ shell_scripts/call_angle_star_sampler.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=eslawler@colostate.edu
 
