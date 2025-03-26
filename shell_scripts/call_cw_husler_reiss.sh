7ee7192951d21c8f9b1d58d80c23cdb5b1f5367c
Liz Lawler <eslawler@gmail.com>
2025-03-26 11:11:34 -0600

    add shell scripts to split husler_reiss into more batches

---
+++ shell_scripts/call_cw_husler_reiss.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --partition=amilan
+#SBATCH --account=csu-general
+#SBATCH --chdir=/scratch/alpine/eslawler@colostate.edu/gaugeDep/
+#SBATCH --qos=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=1:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=eslawler@colostate.edu
+
+export TMPDIR=/scratch/alpine/$USER/tmp/
+export TMP=${TMPDIR}
+export TEMP=${TMPDIR}
+export TEMPDIR=${TMPDIR}
+mkdir -p $TMPDIR
+
+start_i=$(( batch * 1))
+end_i=$(( start_i * 1))
+
+source /curc/sw/anaconda3/2023.09/etc/profile.d/conda.sh
+conda activate r_env
+
+Rscript --vanilla samplers/campbell_wadsworth/d2fitting.R \
+${dep_type} ${level} ${start_i} ${end_i}
