a8def6aeca4d75c15a59c110c98ac386fe3eb0ea
Liz Lawler <eslawler@gmail.com>
2025-03-26 09:46:27 -0600

    add shell scripts to split logistic high into more batches since each run takes 30-40mins

---
+++ shell_scripts/run_cw_logistic_high.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# shell script to call sbatch
+#
+# cycle through loop and launch sbatch for every combination
+#
+dep_type="logistic"
+level="high"
+for batch in {0..4} {10..14} {20..24} {29..34} {39..44}; do
+	export dep_type level batch
+
+	sbatch --job-name ${dep_type}_${level}_${batch}_cw_fits_preds \
+		--output="./shell_scripts/console_output/%x_%j.txt" \
+		shell_scripts/call_cw_fit_and_pred.sh
+
+	sleep 1
+done
