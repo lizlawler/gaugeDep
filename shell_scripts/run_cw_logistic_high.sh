7516b051eee31fe21fbcca92859c77ee1a6934d4
Liz Lawler <eslawler@gmail.com>
2025-03-26 09:55:19 -0600

    fix call to shell script in 'run' script of mini batches of logistic high runs

--- shell_scripts/run_cw_logistic_high.sh
+++ shell_scripts/run_cw_logistic_high.sh
@@ -10,7 +10,7 @@ for batch in {0..4} {10..14} {20..24} {29..34} {39..44}; do
 
 	sbatch --job-name ${dep_type}_${level}_${batch}_cw_fits_preds \
 		--output="./shell_scripts/console_output/%x_%j.txt" \
-		shell_scripts/call_cw_fit_and_pred.sh
+		shell_scripts/call_cw_logistic_high.sh
 
 	sleep 1
 done
